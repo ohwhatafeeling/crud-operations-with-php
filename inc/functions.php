c8c417fb60c67ae590ae10fdfba6e1105f9683b8
ohwhatafeeling <pyro_michael@hotmail.com>
2021-05-16 20:13:39 +1000

    Show report time broken into projects

--- inc/functions.php
+++ inc/functions.php
@@ -11,18 +11,26 @@ function get_project_list() {
   }
 }
 
-function get_task_list() {
+function get_task_list($filter = null) {
   include('connection.php');
 
+  $sql = 'SELECT tasks.*, projects.title AS project FROM tasks
+  JOIN projects ON tasks.project_id = projects.project_id';
+
+  $orderBy = ' ORDER BY date DESC';
+  if ($filter) {
+    $orderBy = ' ORDER BY projects.title ASC, date DESC';
+  }
+
   try {
-    return $db->query(
-      'SELECT tasks.*, projects.title AS project FROM tasks
-      JOIN projects ON tasks.project_id = projects.project_id'
-    );
+    $results = $db->prepare($sql . $orderBy);
+    $results->execute();
   } catch (Exception $e) {
     echo "Error!: " . $e->getMessage() . "</br>";
     return array();
   }
+
+  return $results->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function add_project($title, $category) {
